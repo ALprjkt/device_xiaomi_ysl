891de050d7206b2f0ebc8038296fc2ec0c5ae107
erfanoabdi <erfangplus@gmail.com>
2022-01-18 09:15:45 +0530

    ysl: Prepare to move to in-tree HALs
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>
    Signed-off-by: ALprjkt <168145100+ALprjkt@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -38,6 +38,9 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_HEIGHT := 1440
 TARGET_SCREEN_WIDTH := 720
 
+QCOM_SOONG_NAMESPACE := \
+    $(LOCAL_PATH)/qcom-caf
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
