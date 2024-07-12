b9322cd3855eb1b2964b952ea06abadb2455e6d2
AL <168145100+ALprjkt@users.noreply.github.com>
2024-07-13 01:08:12 +0700

    ysl: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+rm -rf frameworks/base
+git clone --depth=1 https://github.com/ALprjkt/fwb.git -b Q frameworks/base
+git clone --depth=1 https://github.com/ALprjkt/aosp-clang prebuilts/clang/host/linux-x86/clang-aosp
