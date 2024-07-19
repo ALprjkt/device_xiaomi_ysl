6dfc00550a68ae5c6a14f76c40e7d2eadea33ad4
ALprjkt <168145100+ALprjkt@users.noreply.github.com>
2024-07-19 16:12:48 +0000

    ysl: Switch to azure clang
    Signed-off-by: ALprjkt <168145100+ALprjkt@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
 rm -rf frameworks/base
 git clone --depth=1 https://github.com/ALprjkt/fwb.git -b Q frameworks/base
-git clone --depth=1 https://github.com/ALprjkt/aosp-clang prebuilts/clang/host/linux-x86/clang-aosp
+git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang prebuilts/clang/host/linux-x86/clang-azure
