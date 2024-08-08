267b6a8e722207f1c6289ecfd8ecbced2601dafb
ALprjkt <168145100+ALprjkt@users.noreply.github.com>
2024-08-08 07:44:39 +0000

    ysl: Update vendorsetup
    Signed-off-by: ALprjkt <168145100+ALprjkt@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,2 @@
 rm -rf frameworks/base
 git clone --depth=1 https://github.com/ALprjkt/fwb.git -b Q frameworks/base
-git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang prebuilts/clang/host/linux-x86/clang-azure
